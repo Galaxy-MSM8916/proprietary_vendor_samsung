ad69706bb00b8fc2bab59c49571fe28e8f9c9bea
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-05-27 12:41:27 -0400

    msm8916-common: Regenerate makefiles and blobs

--- msm8916-common/Android.mk
+++ msm8916-common/Android.mk
@@ -69,6 +69,16 @@ LOCAL_MODULE_SUFFIX := .apk
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := TimeService
+LOCAL_MODULE_OWNER := samsung
+LOCAL_SRC_FILES := proprietary/app/TimeService/TimeService.apk
+LOCAL_CERTIFICATE := platform
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := .apk
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := ImsLogger+
 LOCAL_MODULE_OWNER := samsung
 LOCAL_SRC_FILES := proprietary/priv-app/ImsLogger+/ImsLogger+.apk
