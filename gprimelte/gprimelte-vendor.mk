fe60f499a89c2cf9be0629eab5ef5784d8cee58c
Vincent Zvikaramba <zvikaram@teach.cs.toronto.edu>
2018-08-29 17:10:05 -0400

    gprimelte: Rename nfc_nci.msm8916.so -> nfc_nci.vendor.msm8916.so
    Change-Id: I68901474d308535952a0cb83aa9fea3ad9a2e936

--- gprimelte/gprimelte-vendor.mk
+++ gprimelte/gprimelte-vendor.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2017-2018 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -28,7 +28,7 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/gprimelte/proprietary/etc/firmware/nfc_test.bin:system/etc/firmware/nfc_test.bin \
     vendor/samsung/gprimelte/proprietary/etc/sec_s3fwrn5_rfreg.bin:system/etc/sec_s3fwrn5_rfreg.bin \
     vendor/samsung/gprimelte/proprietary/lib/hw/camera.vendor.msm8916.so:system/lib/hw/camera.vendor.msm8916.so \
-    vendor/samsung/gprimelte/proprietary/lib/hw/nfc_nci.msm8916.so:system/lib/hw/nfc_nci.msm8916.so \
+    vendor/samsung/gprimelte/proprietary/vendor/lib/hw/nfc_nci.vendor.msm8916.so:system/vendor/lib/hw/nfc_nci.vendor.msm8916.so \
     vendor/samsung/gprimelte/proprietary/lib/hw/sensors.vendor.msm8916.so:system/lib/hw/sensors.vendor.msm8916.so \
     vendor/samsung/gprimelte/proprietary/lib/libAl_Awb.so:system/lib/libAl_Awb.so \
     vendor/samsung/gprimelte/proprietary/lib/libAl_Awb_Sp.so:system/lib/libAl_Awb_Sp.so \
