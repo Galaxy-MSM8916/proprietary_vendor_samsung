3c840c724276c9551157f394c4d784f51975021f
Vincent Zvikaramba <zvikaram@teach.cs.toronto.edu>
2018-08-29 17:09:16 -0400

    fortunalteub: Rename nfc_nci.msm8916.so -> nfc_nci.vendor.msm8916.so
    Change-Id: Idb71dc74a9bc61f543fc301b2abe9144332b9ec2

--- fortunalteub/fortunalteub-vendor.mk
+++ fortunalteub/fortunalteub-vendor.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2017-2018 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -26,7 +26,7 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/fortunalteub/proprietary/etc/calib_SI_matrix_data_file.dat:system/etc/calib_SI_matrix_data_file.dat \
     vendor/samsung/fortunalteub/proprietary/etc/sec_s3fwrn5_rfreg.bin:system/etc/sec_s3fwrn5_rfreg.bin \
     vendor/samsung/fortunalteub/proprietary/lib/hw/camera.vendor.msm8916.so:system/lib/hw/camera.vendor.msm8916.so \
-    vendor/samsung/fortunalteub/proprietary/lib/hw/nfc_nci.msm8916.so:system/lib/hw/nfc_nci.msm8916.so \
+    vendor/samsung/fortunalteub/proprietary/vendor/lib/hw/nfc_nci.vendor.msm8916.so:system/vendor/lib/hw/nfc_nci.vendor.msm8916.so \
     vendor/samsung/fortunalteub/proprietary/lib/hw/sensors.vendor.msm8916.so:system/lib/hw/sensors.vendor.msm8916.so \
     vendor/samsung/fortunalteub/proprietary/lib/libAl_Awb.so:system/lib/libAl_Awb.so \
     vendor/samsung/fortunalteub/proprietary/lib/libAl_Awb_Sp.so:system/lib/libAl_Awb_Sp.so \
