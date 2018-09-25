116c952f4009a36a6e3d1e11779ba26da8e8e7a5
Vincent Zvikaramba <zvikaram@teach.cs.toronto.edu>
2018-09-25 01:26:30 -0400

    msm8916-common: Add libskia
    Change-Id: If5f6ce284c4e5d943e28f5b9b0ff90cc94250d90

--- msm8916-common/Android.mk
+++ msm8916-common/Android.mk
@@ -39,6 +39,16 @@ LOCAL_MODULE_SUFFIX := .so
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := libskia
+LOCAL_MODULE_OWNER := samsung
+LOCAL_SRC_FILES := proprietary/lib/libskia.so
+LOCAL_MULTILIB := 32
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := vendor.qti.hardware.fm@1.0
 LOCAL_MODULE_OWNER := samsung
 LOCAL_SRC_FILES := proprietary/lib/vendor.qti.hardware.fm@1.0.so
