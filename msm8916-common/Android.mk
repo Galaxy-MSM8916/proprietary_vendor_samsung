54d63130a42b5c67f40f46747e44eb0b1ebe22e3
Vincent Zvikaramba <zvikaram@teach.cs.toronto.edu>
2018-09-25 01:40:22 -0400

    msm8916-common: Add ANT+ libs
    Change-Id: I5a377d8a7e513cfb60bfce3bef4df092932ffc0a

--- msm8916-common/Android.mk
+++ msm8916-common/Android.mk
@@ -19,6 +19,16 @@ LOCAL_PATH := $(call my-dir)
 ifneq ($(filter a3lte a33g a3ulte a5ltechn a5ltectc fortuna3g fortunave3g fortunalteub gprimelte gprimeltexx gprimeltespr gprimeltetfnvzw gprimeltezt gprimeltectc gtelwifiue gtesqltespr gt510wifi j53gxx j5lte j5ltechn j5nlte j5xnlte j5xlte j7ltespr j7ltechn o7prolte on7ltechn serranovelte serranove3g,$(TARGET_DEVICE)),)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := com.qualcomm.qti.ant@1.0
+LOCAL_MODULE_OWNER := samsung
+LOCAL_SRC_FILES := proprietary/lib/com.qualcomm.qti.ant@1.0.so
+LOCAL_MULTILIB := 32
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := libloc_api_v02
 LOCAL_MODULE_OWNER := samsung
 LOCAL_SRC_FILES := proprietary/lib/libloc_api_v02.so
