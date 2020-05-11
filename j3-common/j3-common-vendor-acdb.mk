4a17be7ced9fafa156a0dabbb365e5bac4c0dd68
zqm64 <zqm64@outlook.com>
2020-05-11 16:09:57 +0800

    Add j3-common j3ltectc
    Change-Id: I1114974730a5e926884fee677d3ac51015812b3f

---
+++ j3-common/j3-common-vendor-acdb.mk
@@ -0,0 +1,24 @@
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Note: Take from J3109KES1ARG1 (Android 5.1)
+
+PRODUCT_COPY_FILES += \
+    vendor/samsung/j3-common/acdb/etc/Bluetooth_cal.acdb:system/etc/Bluetooth_cal.acdb \
+    vendor/samsung/j3-common/acdb/etc/General_cal.acdb:system/etc/General_cal.acdb \
+    vendor/samsung/j3-common/acdb/etc/Global_cal.acdb:system/etc/Global_cal.acdb \
+    vendor/samsung/j3-common/acdb/etc/Handset_cal.acdb:system/etc/Handset_cal.acdb \
+    vendor/samsung/j3-common/acdb/etc/Hdmi_cal.acdb:system/etc/Hdmi_cal.acdb \
+    vendor/samsung/j3-common/acdb/etc/Headset_cal.acdb:system/etc/Headset_cal.acdb \
+    vendor/samsung/j3-common/acdb/etc/Speaker_cal.acdb:system/etc/Speaker_cal.acdb
