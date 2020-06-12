fdbdc3bd5bb0b4a18958e5dd720105a8e792eee6
zqm64 <zqm64@outlook.com>
2020-06-12 17:58:11 +0800

    j5x-common: Do not copy common camera blobs for j5xltecmcc
    Change-Id: I19a5b985c0d4345fd613d715aa260ba465725a86
    Signed-off-by: Gegham Zakaryan <zakaryan.2004@outlook.com>

--- j5x-common/j5x-common-vendor.mk
+++ j5x-common/j5x-common-vendor.mk
@@ -17,14 +17,29 @@
 PRODUCT_COPY_FILES += \
     vendor/samsung/j5x-common/proprietary/bin/qseecomd:$(TARGET_COPY_OUT_VENDOR)/bin/qseecomd \
     vendor/samsung/j5x-common/proprietary/etc/Bluetooth_cal.acdb:$(TARGET_COPY_OUT_SYSTEM)/etc/Bluetooth_cal.acdb \
-    vendor/samsung/j5x-common/proprietary/etc/D05QL_s5k5e3yx_module_info.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/D05QL_s5k5e3yx_module_info.xml \
     vendor/samsung/j5x-common/proprietary/etc/General_cal.acdb:$(TARGET_COPY_OUT_SYSTEM)/etc/General_cal.acdb \
     vendor/samsung/j5x-common/proprietary/etc/Global_cal.acdb:$(TARGET_COPY_OUT_SYSTEM)/etc/Global_cal.acdb \
     vendor/samsung/j5x-common/proprietary/etc/Handset_cal.acdb:$(TARGET_COPY_OUT_SYSTEM)/etc/Handset_cal.acdb \
     vendor/samsung/j5x-common/proprietary/etc/Hdmi_cal.acdb:$(TARGET_COPY_OUT_SYSTEM)/etc/Hdmi_cal.acdb \
     vendor/samsung/j5x-common/proprietary/etc/Headset_cal.acdb:$(TARGET_COPY_OUT_SYSTEM)/etc/Headset_cal.acdb \
-    vendor/samsung/j5x-common/proprietary/etc/J05QF_sr552_module_info.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/J05QF_sr552_module_info.xml \
     vendor/samsung/j5x-common/proprietary/etc/Speaker_cal.acdb:$(TARGET_COPY_OUT_SYSTEM)/etc/Speaker_cal.acdb \
+    vendor/samsung/j5x-common/proprietary/vendor/lib/libdiag.so:$(TARGET_COPY_OUT_VENDOR)/lib/libdiag.so \
+    vendor/samsung/j5x-common/proprietary/vendor/lib/libdsutils.so:$(TARGET_COPY_OUT_VENDOR)/lib/libdsutils.so \
+    vendor/samsung/j5x-common/proprietary/vendor/lib/libidl.so:$(TARGET_COPY_OUT_VENDOR)/lib/libidl.so \
+    vendor/samsung/j5x-common/proprietary/vendor/lib/libqcci_legacy.so:$(TARGET_COPY_OUT_VENDOR)/lib/libqcci_legacy.so \
+    vendor/samsung/j5x-common/proprietary/vendor/lib/libqmi_cci.so:$(TARGET_COPY_OUT_VENDOR)/lib/libqmi_cci.so \
+    vendor/samsung/j5x-common/proprietary/vendor/lib/libqmi_client_qmux.so:$(TARGET_COPY_OUT_VENDOR)/lib/libqmi_client_qmux.so \
+    vendor/samsung/j5x-common/proprietary/vendor/lib/libqmi_common_so.so:$(TARGET_COPY_OUT_VENDOR)/lib/libqmi_common_so.so \
+    vendor/samsung/j5x-common/proprietary/vendor/lib/libqmi_csi.so:$(TARGET_COPY_OUT_VENDOR)/lib/libqmi_csi.so \
+    vendor/samsung/j5x-common/proprietary/vendor/lib/libqmi_encdec.so:$(TARGET_COPY_OUT_VENDOR)/lib/libqmi_encdec.so \
+    vendor/samsung/j5x-common/proprietary/vendor/lib/libqmiservices.so:$(TARGET_COPY_OUT_VENDOR)/lib/libqmiservices.so \
+    vendor/samsung/j5x-common/proprietary/vendor/lib/libqmi.so:$(TARGET_COPY_OUT_VENDOR)/lib/libqmi.so \
+    vendor/samsung/j5x-common/proprietary/vendor/lib/libQSEEComAPI.so:$(TARGET_COPY_OUT_VENDOR)/lib/libQSEEComAPI.so
+
+ifeq ($(filter j5xltecmcc,$(TARGET_DEVICE)),)
+PRODUCT_COPY_FILES += \
+    vendor/samsung/j5x-common/proprietary/etc/D05QL_s5k5e3yx_module_info.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/D05QL_s5k5e3yx_module_info.xml \
+    vendor/samsung/j5x-common/proprietary/etc/J05QF_sr552_module_info.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/J05QF_sr552_module_info.xml \
     vendor/samsung/j5x-common/proprietary/etc/V13QL_s5k3l2xx_module_info.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/V13QL_s5k3l2xx_module_info.xml \
     vendor/samsung/j5x-common/proprietary/lib/hw/camera.vendor.msm8916.so:$(TARGET_COPY_OUT_SYSTEM)/lib/hw/camera.vendor.msm8916.so \
     vendor/samsung/j5x-common/proprietary/lib/libAl_Awb.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libAl_Awb.so \
@@ -55,7 +70,6 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/j5x-common/proprietary/lib/libmmcamera2_stats_algorithm.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libmmcamera2_stats_algorithm.so \
     vendor/samsung/j5x-common/proprietary/lib/libmmcamera_cac2_lib_13M.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libmmcamera_cac2_lib_13M.so \
     vendor/samsung/j5x-common/proprietary/lib/libmmcamera_wavelet_lib.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libmmcamera_wavelet_lib.so \
-    vendor/samsung/j5x-common/proprietary/vendor/lib/libQSEEComAPI.so:$(TARGET_COPY_OUT_VENDOR)/lib/libQSEEComAPI.so \
     vendor/samsung/j5x-common/proprietary/vendor/lib/libactuator_zc533_camcorder.so:$(TARGET_COPY_OUT_VENDOR)/lib/libactuator_zc533_camcorder.so \
     vendor/samsung/j5x-common/proprietary/vendor/lib/libactuator_zc533_camera.so:$(TARGET_COPY_OUT_VENDOR)/lib/libactuator_zc533_camera.so \
     vendor/samsung/j5x-common/proprietary/vendor/lib/libchromatix_s5k5e3yx_wide_f1_9_common.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_s5k5e3yx_wide_f1_9_common.so \
@@ -74,9 +88,6 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/j5x-common/proprietary/vendor/lib/libchromatix_sr552_vt_15fps.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_sr552_vt_15fps.so \
     vendor/samsung/j5x-common/proprietary/vendor/lib/libchromatix_sr552_vt_30fps.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_sr552_vt_30fps.so \
     vendor/samsung/j5x-common/proprietary/vendor/lib/libchromatix_sr552_zslshot.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_sr552_zslshot.so \
-    vendor/samsung/j5x-common/proprietary/vendor/lib/libdiag.so:$(TARGET_COPY_OUT_VENDOR)/lib/libdiag.so \
-    vendor/samsung/j5x-common/proprietary/vendor/lib/libdsutils.so:$(TARGET_COPY_OUT_VENDOR)/lib/libdsutils.so \
-    vendor/samsung/j5x-common/proprietary/vendor/lib/libidl.so:$(TARGET_COPY_OUT_VENDOR)/lib/libidl.so \
     vendor/samsung/j5x-common/proprietary/vendor/lib/libmmcamera2_c2d_module.so:$(TARGET_COPY_OUT_VENDOR)/lib/libmmcamera2_c2d_module.so \
     vendor/samsung/j5x-common/proprietary/vendor/lib/libmmcamera2_cpp_module.so:$(TARGET_COPY_OUT_VENDOR)/lib/libmmcamera2_cpp_module.so \
     vendor/samsung/j5x-common/proprietary/vendor/lib/libmmcamera2_frame_algorithm.so:$(TARGET_COPY_OUT_VENDOR)/lib/libmmcamera2_frame_algorithm.so \
@@ -105,12 +116,5 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/j5x-common/proprietary/vendor/lib/libmmcamera_zc533_eeprom.so:$(TARGET_COPY_OUT_VENDOR)/lib/libmmcamera_zc533_eeprom.so \
     vendor/samsung/j5x-common/proprietary/vendor/lib/libmmipl.so:$(TARGET_COPY_OUT_VENDOR)/lib/libmmipl.so \
     vendor/samsung/j5x-common/proprietary/vendor/lib/liboemcamera.so:$(TARGET_COPY_OUT_VENDOR)/lib/liboemcamera.so \
-    vendor/samsung/j5x-common/proprietary/vendor/lib/libqcci_legacy.so:$(TARGET_COPY_OUT_VENDOR)/lib/libqcci_legacy.so \
-    vendor/samsung/j5x-common/proprietary/vendor/lib/libqmi.so:$(TARGET_COPY_OUT_VENDOR)/lib/libqmi.so \
-    vendor/samsung/j5x-common/proprietary/vendor/lib/libqmi_cci.so:$(TARGET_COPY_OUT_VENDOR)/lib/libqmi_cci.so \
-    vendor/samsung/j5x-common/proprietary/vendor/lib/libqmi_client_qmux.so:$(TARGET_COPY_OUT_VENDOR)/lib/libqmi_client_qmux.so \
-    vendor/samsung/j5x-common/proprietary/vendor/lib/libqmi_common_so.so:$(TARGET_COPY_OUT_VENDOR)/lib/libqmi_common_so.so \
-    vendor/samsung/j5x-common/proprietary/vendor/lib/libqmi_csi.so:$(TARGET_COPY_OUT_VENDOR)/lib/libqmi_csi.so \
-    vendor/samsung/j5x-common/proprietary/vendor/lib/libqmi_encdec.so:$(TARGET_COPY_OUT_VENDOR)/lib/libqmi_encdec.so \
-    vendor/samsung/j5x-common/proprietary/vendor/lib/libqmiservices.so:$(TARGET_COPY_OUT_VENDOR)/lib/libqmiservices.so \
     vendor/samsung/j5x-common/proprietary/vendor/lib/libqomx_jpegenc.so:$(TARGET_COPY_OUT_VENDOR)/lib/libqomx_jpegenc.so
+endif
